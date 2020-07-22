8a3dc2340dcf2c263ae2af84b705d1ac72dfe4d2
babyazalea <usun12@hotmail.com>
2020-07-23 00:20:46 +0900

    Add JavaScript code in main.js for interaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,9 @@ import "../scss/styles.scss";
 const something = async () => {
   console.log("something");
 };
+
+const npCheckbox = document.querySelector(".np__checkbox");
+
+const npCheckboxChecked = () => {};
+
+npCheckbox.addEventListener("change", npCheckboxChecked, false);
